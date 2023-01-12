425c638eb44a3af7a7e5b8ad387a9282f21b4769
Micro0610 <blizzardbon@gmail.com>
2023-01-12 16:10:17 +0000

    UPDATE_main.py
    Át írtam a nagyrésze roulettet, és azt be is fejeztem.

--- main.py
+++ main.py
@@ -9,38 +9,55 @@ zseton = 500 #Kezdő zsteon szám
 
 
 while(True):
-    print("- Roulett (1) \n- BlackJack (2) \n")
+    print("- Roulett (1) \n- BlackJack (2) \n- Darts (3)\n")
     print("\nZsetonok száma:", zseton)
     jatek = int(input("Kérem adja meg annak a játéknak a számát amivel játszani szeretne: "))
-    
+    if zseton == 0:
+        os.system('cls')
+        print("Nincs több zsetonja. Szeretne még 500-at? (I/N)\n")
+        valasz = input()
+        if valasz == "I" or valasz == "i":
+            zseton = zseton + 500
+        elif valasz == "N" or valasz == "n":
+            break
     if jatek == 1:
         os.system('cls')
-        szamok = []
         print("\nA Roulette-t választotta!")
         while(True):
             os.system('cls')
             while(True):
                 print("\nKérem válasszon számot!\n\nPiros: 2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36\nFekete: 1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35\nZöld: 0")
                 szam = int(input())
-                if szam in szamok:
-                    print("Kérem adjon meg olyan számot amire még nem tett tétet!")
-                    print(szamok)
-                    time.sleep(4)
-                    break
-                szamok.append(szam)
                 print("Zsetonok száma = ", zseton)
                 tet = int(input("Kérem adja meg a tét mennyiségét!\n"))
                 zseton = zseton - tet
-                print("Választott számok: ", szamok)
-                valasz = input("Szeretne még másik számra tétet rakni? (I/N)")
-                if valasz == "I" or valasz == "i":
-                    continue
-                elif valasz == "N" or valasz == "n":
+                randomszam = random.randint(1,37)
+                if szam == randomszam:
+                    zseton = zseton + (tet * 2)
+                    print("Nyertél", tet * 2, "zsetont")
+                    valasz = input("Szeretne tovább játszani? (I/N)")
+                    if valasz == "I" or valasz == "i":
+                        break
+                    elif valasz == "N" or valasz == "n":
+                        break
                     break
-            if valasz == "N" or valasz == "n":
-                os.system("cls")
+                elif szam != randomszam:
+                    print("Sajnos nem nyertél!")
+                    valasz = input("Szeretne tovább játszani? (I/N)")
+                    if valasz == "I" or valasz == "i":
+                        break
+                    elif valasz == "N" or valasz == "n":
+                        os.system('cls')
+                        break
+                    break
                 break
-                
+            break
+
     if jatek == 2:
         os.system('cls')
         print("\nA BlackJack-et választotta!")
+
+
+    if jatek == 3:
+        os.system('cls')
+        print("\nA Darts-ot válaszotta!")
